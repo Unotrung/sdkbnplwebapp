a04b19cd3500f927fe8a47e505f743dba5498eef
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-07-25 16:15:50 +0700

    Update 16:15 25/07/2022

--- frontend/index.js
+++ frontend/index.js
@@ -670,7 +670,7 @@ function showAllTenor(element, nCount = 0) {
 
     for (var i = 0; i < count; i++) {
         html += `
-        <div class='voolo-intro tenor-list' data-id='${tenors[i]._id}' onchange='selectTenor(this)'>
+        <div class='voolo-intro tenor-list' data-id='${tenors[i]._id}' onclick='selectTenor(this)'>
             <div class='tenor-item'>
                 <div class="tenor-head">
                     <div class='sub4'>${lang.showAllTenor.term_1}</div class='sub4'>
