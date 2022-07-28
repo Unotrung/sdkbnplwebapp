af455da868ae75476cfc695e046befb1b3fbf6ee
Unotrung <trung.ta@wolfconsulting.com.sg>
2022-07-28 17:13:58 +0700

    conlict 1

--- helper/translate/lang.vn.js
+++ helper/translate/lang.vn.js
@@ -23,221 +23,243 @@ lang = {
         error_incorrect_OTP_5 : "Mã OTP không chính xác (5/5).</br>Tài khoản của bạn đã bị khóa, thử lại sau 60 phút.",
         error_incorrect_pin_5 : "Mã PIN không chính xác (5/5).</br>Tài khoản của bạn đã bị khóa, thử lại sau 60 phút.",
     },
-    configUi: {
-        slogan1: "Mua trước Trả sau Không khoản trả trước",
-        slogan2: "Nhẹ nhàng với 0% lãi suất",
-        voolo_intro: "VOOLO giúp bạn:",
-        feature_voolo_intro_1: "Mua sắm không giới hạn",
-        feature_voolo_intro_2: "Thanh toán linh hoạt",
-        feature_voolo_intro_3: "Hoàn tiền ngay chỉ trong 1 ngày",
+
+    //configUi
+    configUi : {
+        slogan : "<p>Mua trước Trả sau Không khoản trả trước</p><p>Nhẹ nhàng với 0% lãi suất </p>",
+        voolo_intro : "VOOLO giúp bạn:",
+        sub_voolo_intro : "<ul><li>Mua sắm không giới hạn </li><li>Thanh toán linh hoạt </li><li>Hoàn tiền ngay chỉ trong 1 ngày </li></ul>",
     },
+
+
+
     //showCircularProgressbar
     showCircularProgressbar : {
         verifying : "Đang trong tiến trình xác minh thông tin",
         note : "Lưu ý: Tiến trình xác minh hồ sơ có thể mất từ 5-15 phút",
     },
-    
-    showUICheckNid: {
-        type_nid: "Vui lòng nhập số CMND/CCCD",
-        capture_selfie: "Chụp ảnh chân dung",
-        invalid_nid: "Số CMND/CCCD không hợp lệ",
-        empty_nid: "Vui lòng nhập CMND/CCCD",
-        registered_nid: "Số CMND/CCCD đã được đăng ký.",
+
+    //showUICheckNid
+    showUICheckNid : {
+        input_nid : "Vui lòng nhập số CMND/CCCD",
+        capture_selfie : "Chụp ảnh chân dung",
+        error_nid : "Số CMND/CCCD không hợp lệ",
+        error_nid_null : "Vui lòng nhập CMND/CCCD",
+        error_nid_registered : "CMND/CCCD đã được đăng ký.",
+        error_nid_ : "CMND/CCCD không hợp lệ !",
     },
-    
-    captureNidFrontAndBack: {
-        capture_nid: "Chụp ảnh CMND/CCCD",
-        capture_front_nid: "CMND mặt trước",
-        capture_back_nid: "CMND mặt sau",
+
+    //captureNidFrontAndBack
+    captureNidFrontAndBack : {
+        capture_nid : "Chụp ảnh CMND/CCCD",
+        capture_front_nid : "CMND mặt trước",
+        capture_back_nid : "CMND mặt sau",
+        capture_nid : "Chụp ảnh CMND/CCCD",
     },
-    
-    cutStringData: {
-        notification: "Thông báo",
-        not_match_nid_front: "Số CMND/CCCD trước không trùng khớp,<br/> đề nghị chụp lại",
-        error_capture_front_nid: "Lỗi chụp CMND/CCCD<br/>Mặt trước, đề nghị chụp lại",
+
+    //cutStringData
+    cutStringData : {
+        notification : "Thông báo",
+        error_nid_front : "CMND/CCCD trước không trùng khớp,<br/> đề nghị chụp lại",
+        error_capture_front_nid : "Lỗi chụp CMND/CCCD<br/>Mặt trước, đề nghị chụp lại",
     },
-    
-    makeFaceMatchCall: {
-        not_match_selfie: "Ảnh chân dung và ảnh trên CMND/CCCD không khớp, đề nghị chụp lại ảnh.",
+
+    //makeFaceMatchCall
+    makeFaceMatchCall : {
+        error_selfie_notmatch : "Ảnh chân dung và ảnh trên CMND/CCCD không khớp, đề nghị chụp lại ảnh. ", 
     },
-    
-    LaunchFaceCaptureScreen: {
-        capture_selfie: "Chụp ảnh chân dung",
-        use_phone_and_device: "Bạn đang chụp ảnh chân dung để đăng ký tài khoản (KYC). Vui lòng sử dụng số điện thoại và thiết bị của bạn.",
-        capture_rules: "Vui lòng KHÔNG đội nón, đeo kính, khẩu trang, chọn nơi có đủ ánh sáng để chụp ảnh",
-        away_camera: "Cách xa camera",
-        capture_now: "Chụp ảnh ngay",
-        capture_review: "faceCaptureReviewTitle",
+
+    //LaunchFaceCaptureScreen
+    LaunchFaceCaptureScreen : {
+        capture_selfie : "Chụp ảnh chân dung",
+        use_phone : "Bạn đang chụp ảnh chân dung để đăng ký tài khoản (KYC). Vui lòng sử dụng số điện thoại và thiết bị của bạn.",
+        capture_rules : "Vui lòng KHÔNG đội nón, đeo kính, khẩu trang, chọn nơi có đủ ánh sáng để chụp ảnh",
+        away_camera : "Cách xa camera",
+        capture_now : "Chụp ảnh ngay",
+        capture_review : "faceCaptureReviewTitle",
+        FaceCaptureReviewBottomDescription : "Bạn đang chụp ảnh chân dung để đăng ký tài khoản (KYC). Vui lòng sử dụng số điện thoại và thiết bị của bạn.",
     },
-    
-    LaunchDocumentCaptureScreen: {
-        capture_nid: "Chụp ảnh CMND/CCCD",
-        choose_place: "Chọn nơi đủ ánh sáng và đưa CMND/CCCD trong khung hình",
-        capture_front: "Chụp ảnh mặt trước",
-        capture_back: "Chụp ảnh mặt sau",
-        unsatisfactory_picture: "Hình ảnh không đạt yêu cầu",
+
+    //LaunchDocumentCaptureScreen
+    LaunchDocumentCaptureScreen : {
+        capture_nid : "Chụp ảnh CMND/CCCD",
+        choose_place : "Chọn nơi đủ ánh sáng và đưa CMND/CCCD trong khung hình",
+        capture_front : "Chụp ảnh mặt trước",
+        capture_back : "Chụp ảnh mặt sau",
+        Unsatisfactory_picture: "Hình ảnh không đạt yêu cầu",
     },
-    
-    showAllTenor: {
-        select_payment_term: "Vui lòng chọn kì hạn thanh toán",
-        term_1: "KÌ HẠN 1",
-        product_price: "Giá sản phẩm:",
-        conversion_fee: "Phí chuyển đổi:",
-        payment_time: "Thời gian thanh toán: ",
-        payment_type: "ngày",
-        show_more: "Hiển thị thêm",
+
+    //showAllTenor
+    showAllTenor : {
+        select_payment_term : "Vui lòng chọn kì hạn thanh toán",
+        term_1 : "KÌ HẠN 1",
+        product_price : "Giá sản phẩm:",
+        conversion_fee : "Phí chuyển đổi:",
+        payment_time : "Thời gian thanh toán: ",
+        payment_type : "ngày",
+        show_more : "Hiển thị thêm",
     },
-    
-    deleteImage: {
-        delete_img_success: "Xóa ảnh selfie thành công !",
-        not_found_img: "Không tìm thấy ảnh để xóa !",
-        delete_front_img: "Xóa mặt trước selfie thành công !",
-        delete_back_img: "Xóa mặt sau selfie thành công !",
+
+    //deleteImage
+    deleteImage : {
+        delete_img_success : "Xóa ảnh selfie thành công !",
+        not_found_img : "Không tìm thấy ảnh để xóa !",
+        delete_front_img : "Xóa mặt trước selfie thành công !",
+        delete_back_img : "Xóa mặt sau selfie thành công !",
     },
-    
-    onHandleChangeDataCity: {
-        type_district: "Vui lòng nhập quận, huyện",
-        type_city: "Vui lòng nhập thành phố, tỉnh",
+
+    //onHandleChangeDataCity
+    onHandleChangeDataCity : {
+        input_district : "Vui lòng nhập quận, huyện",
+        input_city : "Vui lòng nhập thành phố, tỉnh",
     },
-    
-    showDataInform: {
-        type_info: "Nhập thông tin cá nhân",
-        enter_all_field: "Vui lòng điền các trường thông tin bên dưới",
-        info: "Thông tin cá nhân",
-        info_name: "Họ và tên",
-        type_name: "Vui lòng nhập họ và tên",
-        info_phone: "Số điện thoại",
-        type_phone: "Vui lòng nhập số điện thoại",
-        info_birth: "Ngày sinh",
-        info_gender: "Giới tính",
-        choose_gender: "Chọn giới tính",
-        gender_man: "Nam",
-        gender_woman: "Nữ",
-        info_nid: "Số CMND/CCCD",
-        type_nid: "Vui lòng nhập CMND/CCCD",
-        info_doi: "Ngày cấp",
-        info_doe: "Ngày hết hạn",
-        current_address: "Địa chỉ hiện tại",
-        info_city: "Thành phố/Tỉnh",
-        choose_city: "Chọn thành phố/tỉnh",
-        info_district: "Quận/Huyện",
-        choose_district: "Chọn quận/huyện",
-        info_ward: "Phường/Xã",
-        choose_ward: "Chọn phường/xã",
-        type_ward: "Vui lòng nhập phường, xã",
-        info_street: "Đường",
-        type_street: "Vui lòng nhập đường",
-        res_address: "Địa chỉ tạm trú",
-        reference_info: "Thông tin tham chiếu",
-        relationship: "Mối quan hệ",
-        type_relation: "Vui lòng nhập mối quan hệ",
-        info_name_ref: "Họ và tên",
-        type_name_ref: "Vui lòng nhập họ và tên",
-        info_phone_ref: "Số điện thoại",
-        type_phone_ref: "Vui lòng nhập Số điện thoại",
-        capture_selfie: "Chụp ảnh chân dung",
-        invalid_phone: "Số điện thoại không hợp lệ",
-        invalid_nid: "CMND/CCCD không hợp lệ",
-        same_phone: "Số điện thoại tham chiếu trùng số điện thoại đăng ký",
+
+    //showDataInform
+    showDataInform : {
+        input_info : "Nhập thông tin cá nhân",
+        fill_fields_below : "Vui lòng điền các trường thông tin bên dưới",
+        info : "Thông tin cá nhân",
+        info_name : "Họ và tên",
+        input_name : "Vui lòng nhập họ và tên",
+        info_phone : "Số điện thoại",
+        input_phone : "Vui lòng nhập số điện thoại",
+        info_birth : "Ngày sinh",
+        info_gender : "Giới tính",
+        choose_gender : "Chọn giới tính",
+        gender_man : "Nam",
+        gender_woman : "Nữ",
+        nid : "Số CMND/CCCD",
+        input_nid : "Vui lòng nhập CMND/CCCD",
+        doi : "Ngày cấp",
+        doe : "Ngày hết hạn",
+        cur_address : "Địa chỉ hiện tại",
+        city : "Thành phố/Tỉnh",
+        choose_city : "Chọn thành phố/tỉnh",
+        district : "Quận/Huyện",
+        choose_district : "Chọn quận/huyện",
+        wards : "Phường/Xã",
+        choose_wards : "Chọn phường/xã",
+        input_wards : "Vui lòng nhập phường, xã",
+        street : "Đường",
+        input_street : "Vui lòng nhập đường",
+        res_address : "Địa chỉ tạm trú",
+        reference_info : "Thông tin tham chiếu",
+        relationship : "Mối quan hệ",
+        input_relation : "Vui lòng nhập mối quan hệ",
+        info_name_ref : "Họ và tên",
+        input_name_ref : "Vui lòng nhập họ và tên",
+        info_phone_ref : "Số điện thoại",
+        input_phone_ref : "Vui lòng nhập Số điện thoại",
+        capture_selfie : "Chụp ảnh chân dung",
+        error_phone : "Số điện thoại không hợp lệ",
+        error_nid : "CMND/CCCD không hợp lệ",
+        error_same_phone : "Số điện thoại tham chiếu trùng số điện thoại đăng ký",
     },
-    
-    showConfirmDataInform: {
-        check_info: "Đối soát thông tin",
-        confirm_info: "Vui lòng xác nhận các thông tin bên dưới",
-        button_back: "Quay lại",
-        button_confirm: "Xác nhận",
+
+    //showConfirmDataInform
+    showConfirmDataInform : {
+        check_info : "Đối soát thông tin",
+        confirm_info : "Vui lòng xác nhận các thông tin bên dưới",
+        button_back : "Quay lại",
+        button_confirm : "Xác nhận",
     },
-    
-    forgotPinPhone: {
-        phone_number: "Số điện thoại",
-        empty_phone: "Vui lòng nhập số điện thoại để tiếp tục",
-        invalid_phone: "Số điện thoại không hợp lệ",
-        type_phone: "Vui lòng nhập số điện thoại",
+
+    //forgotPinPhone
+    forgotPinPhone : {
+        phone_number : "Số điện thoại",
+        input_phone_null : "Vui lòng nhập số điện thoại để tiếp tục",
+        error_phone : "Số điện thoại không hợp lệ",
+        input_phone : "Vui lòng nhập số điện thoại",
     },
-    
-    forgotPinNid: {
-        empty_nid: "Vui lòng nhập số CMND/CCCD",
-        invalid_nid: "Số CMND/CCCD không hợp lệ",
-        type_nid: "Vui lòng nhập CMND/CCCD",
-        error_OTP: "Mã OTP không chính xác",
-        error_acc: "Tài khoản chưa tồn tại. Vui lòng đăng ký",
-        error_nid_retype: "Số CMND/CCCD không đúng. Vui lòng nhập số khác.",
+
+    //forgotPinNid
+    forgotPinNid : {
+        input_nid_null : "Vui lòng nhập số CMND/CCCD",
+        error_nid : "Số CMND/CCCD không hợp lệ",
+        input_nid : "Vui lòng nhập CMND/CCCD",
+        error_OTP : "Mã OTP không chính xác",
+        error_acc : "Tài khoản chưa tồn tại. Vui lòng đăng ký",
+        error_nid_retype : "Số CMND/CCCD không đúng. Vui lòng nhập số khác.",
     },
-    
-    showFormPincode: {
-        type_pin: "Nhập mã PIN",
-        type_payment: "Vui lòng nhập mã PIN để thanh toán",
-        info_pin: "Mã PIN",
-        info_pin_verify: "Vui lòng nhập mã PIN để xác thực thông tin",
-        pin_verify_payment: "Vui lòng nhập mã PIN để xác thực thanh toán",
-        forgot_pin: "Quên mã PIN?",
-        click: "Nhấn vào đây",
-        error_type_pin: "Vui lòng nhập mã PIN",
-        invalid_phone: "Số điện thoại không hợp lệ",
-        error_incorrect_pin: "Mã PIN không chính xác",
-        error_incorrect_pin_5: "Mã PIN không chính xác (5/5).</br>Tài khoản của bạn đã bị khóa, thử lại sau 60 phút.",
+
+    //showFormPincode
+    showFormPincode : {
+        pin_input : "Nhập mã PIN",
+        pls_payment : "Vui lòng nhập mã PIN để thanh toán",
+        pin : "Mã PIN",
+        pin_verify_info : "Vui lòng nhập mã PIN để xác thực thông tin",
+        pin_verify_payment : "Vui lòng nhập mã PIN để xác thực thanh toán",
+        forgot_pin : "Quên mã PIN? ",
+        click : "Nhấn vào đây",
+        error_input_pin : "Vui lòng nhập mã PIN",
+        error_phone : "Số điện thoại không hợp lệ",
+        error_incorrect_pin : "Mã PIN không chính xác",
+        error_incorrect_pin_5 : "Mã PIN không chính xác (5/5).</br>Tài khoản của bạn đã bị khóa, thử lại sau 60 phút.",
     },
-    
-    showFormSetupPin: {
-        setup_pin: "Cài đặt mã PIN của bạn",
-        retype_pin: "Nhập lại mã PIN",
-        notification_success: "Thành công",
-        send_info: "Đã gửi thông tin thành công",
-        notification_unsuccess: "Không thành công",
-        error_send_info: "Lỗi gửi thông tin, vui lòng thử lại!",
-        not_match_pin: "Mã PIN không trùng khớp",
+
+    //showFormSetupPin
+    showFormSetupPin : {
+        setup_pin : "Cài đặt mã PIN của bạn",
+        retype_pin : "Nhập lại mã PIN",
+        notification_success : "Thành công",
+        send_info : "Đã gửi thông tin thành công",
+        notification_unsuccess : "Không thành công",
+        error_send_info : "Lỗi gửi thông tin, vui lòng thử lại!",
+        pin_notmatch : "Mã PIN không trùng khớp",
     },
-    
-    resendOTP: {
-        OTP: "Mã OTP của bạn là: ",
+
+    //resendOTP
+    resendOTP : {
+        OTP : "Mã OTP của bạn là: ",
     },
-    
-    showFormVerifyOTP: {
-        type_OTP: "Nhập OTP",
-        sent_OTP: "Mã OTP đã được gửi đến số điện thoại",
-        not_receive_OTP: "Không nhận được OTP?",
-        resend_OTP: "Gửi lại OTP ",
-        error_incorrect_OTP: "Mã OTP không chính xác",
-        error_incorrect_OTP_5: "Mã OTP không chính xác (5/5). Vui lòng thử lại sau 60 phút",
-        error_expired_OTP: "Mã OTP hết hiệu lực",
+
+    //showFormVerifyOTP
+    showFormVerifyOTP : {
+        input_OTP : "Nhập OTP",
+        sent_OTP : "Mã OTP đã được gửi đến số điện thoại ",
+        OTP_not_receive : "Không nhận được OTP?",
+        resend_OTP : "Gửi lại OTP ",
+        error_incorrect_OTP : "Mã OTP không chính xác",
+        error_incorrect_OTP_5 : "Mã OTP không chính xác (5/5). Vui lòng thử lại sau 60 phút",
+        error_expired_OTP : "Mã OTP hết hiệu lực",  
     },
-    
-    showContract: {
-        contract: "Mẫu hợp đồng",
-        confirm_contract: "Tôi đồng ý với Điều kiện và Điều khoản hợp đồng",
-        send_OTP_to_phone: "Vui lòng gửi OTP xác nhận về số điện thoại đã đăng ký VOOLO của tôi",
+
+    //showContract
+    showContract : {
+        contract : "Mẫu hợp đồng",
+        confirm_contract : "Tôi đồng ý với Điều kiện và Điều khoản hợp đồng",
+        send_OTP_to_phone : "Vui lòng gửi OTP xác nhận về số điện thoại đã đăng ký VOOLO của tôi",
     },
-    
-    customerInfo: {
-        congratulation: "Chúc mừng bạn, với hạn mức tín dụng này bạn đủ điều kiện để hoàn tất đơn hàng.",
-        unfortunately: "Rất tiếc, với hạn mức tín dụng này bạn không đủ điều kiện để hoàn tất đơn hàng.",
-        limit_credit: "Hạn mức tín dụng của bạn là : ",
-        yeah: "ơi!"
+
+    //customerInfo
+    customerInfo : {
+        congatulation : "Chúc mừng bạn, với hạn mức tín dụng này bạn đủ điều kiện để hoàn tất đơn hàng.",
+        unfortunately : "Rất tiếc, với hạn mức tín dụng này bạn không đủ điều kiện để hoàn tất đơn hàng.",
+        limit_credit : "Hạn mức tín dụng của bạn là : ",
     },
-    
-    showProcessPipeline: {
-        buy_now_pay_later: "Chào mừng bạn đến với quy trình đăng ký Mua trước Trả sau",
-        cus_info: "Thông tin khách hàng",
-        setup_pin: "Cài đặt PIN",
-        sign_onl: "Ký điện tử",
-        verify_info: "Xác minh thông tin",
-        complete: "Hoàn thành",
-    },
-    
-    messageScreen: {
-        registered_success: "Bạn đã đăng ký thành công",
-        registered_unsuccess: "Đăng ký không thành công",
-        update_fail_pin: "Cập nhật mã PIN không thành công",
-        update_success_pin: "Cập nhật mã PIN thành công",
-        buy_success: "Chúc mừng bạn đã <span>mua hàng thành công</span>",
-        buy_fail: "Mua hàng không thành công",
-        payment_code: "Mã thanh toán: ",
-        call_switchboard: "Vui lòng thử lại hoặc liên hệ <b>1900xxx</b> để được hỗ trợ.",
-        button_retry: "Thử lại",
-        click: "Bấm vào",
-        here: "đây",
-        return: "để quay trở lại.",
-        auto_return: "Tự động trở lại trang mua hàng sau ",
+
+    //showProcessPipeline
+    showProcessPipeline : {
+        pay_first_buy_later : "Chào mừng bạn đến với quy trình đăng ký Mua trước Trả sau",
+        cus_info : "Thông tin khách hàng",
+        setup_pin : "Cài đặt PIN",
+        sign_onl : "Ký điện tử",
+        verify_info : "Xác minh thông tin",
+        complete : "Hoàn thành",
+    }, 
+
+    //messageScreen
+    messageScreen : {
+        registered_success : "Bạn đã đăng ký thành công",
+        registered_unsuccess : "Đăng ký không thành công",
+        update_fail_pin : "Cập nhật mã PIN không thành công",
+        update_success_pin : "Cập nhật mã PIN thành công",
+        buy_success : "Chúc mừng bạn đã <span>mua hàng thành công</span>",
+        buy_fail : "Mua hàng không thành công",
+        payment_code : "Mã thanh toán: ",
+        call_switchboard : "Vui lòng thử lại hoặc liên hệ <b>1900xxx</b> để được hỗ trợ.",
+        button_retry : "Thử lại",
+        callback : `Bấm vào <a class="ahref" href="${DOMAIN}" style='width:auto'>đây</a> để quay trở lại. <span>Tự động trở lại trang mua hàng sau <c class='coutdown'>5</c>s.</span>`
     },
 
     //showUseGuideSelfy
